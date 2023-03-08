f7d0868cee182cc5df8b85f4415df6b18e95bac0
Leandro339 <127158129+Leandro339@users.noreply.github.com>
2023-03-08 09:51:12 -0300

    Add files via upload

---
+++ infomacion.js
@@ -0,0 +1,4 @@
+function Hola() {
+      document.write( "Curso en Floreria Las Rosas Enero 2019 hasta Marzo 2020")
+      ("Curso de Radio Online en Casa Cultural Belgrano Marzo 2021 hasta Junio 2021 ")
+}
